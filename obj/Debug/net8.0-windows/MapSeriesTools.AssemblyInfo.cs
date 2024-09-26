75a703b412354d370053985004bb17d4d7376709
DanielRubianes <DanielRubianes1352@gmail.com>
2024-09-26 16:20:00 -0400

    Next Page, Previous Page buttons; working on property window to select map series

--- obj/Debug/net8.0-windows/MapSeriesTools.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MapSeriesTools.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MapSeriesTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d37a18e6a939ebac82cc7df5fb60f03321c9fef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c8dfcef7f7210db9054b0238f9011d6be7a3456")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapSeriesTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapSeriesTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
